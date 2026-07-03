4e2eb1baad40021d3d15160e764b6996159b3e0f
eric1981 <saints0256@gmail.com>
2026-07-03 10:32:57 +0800

    fix: limit shadow-fix to specific domains, avoid anti-bot detection on all pages

--- entrypoints/shadow-fix.content.ts
+++ entrypoints/shadow-fix.content.ts
@@ -1,5 +1,5 @@
 export default defineContentScript({
-  matches: ['<all_urls>'],
+  matches: ['https://creator.xiaohongshu.com/*', 'https://post.smzdm.com/*', 'https://zhiyou.smzdm.com/*', 'https://baijiahao.baidu.com/*', 'https://mp.toutiao.com/*'],
   runAt: 'document_start',
   main() {
     const script = document.createElement('script');
